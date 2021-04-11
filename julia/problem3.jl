02d55f8bbf5058eb61c4f1331ca271cf86d6ab1f
Tyler Vick <tyler.j.vick@accenture.com>
2021-04-11 18:49:37 -0400

    New solution for problem  3

--- julia/problem3.jl
+++ julia/problem3.jl
@@ -1,31 +1,57 @@
-## Attempt 1
-function v1(target)
-  primeset = []
-  i = 2
-  while i < √target
-    if target % i == 0 && !any(i .% primeset .== 0)
-      append!(primeset, i)
-    end
-    i += 1
-  end
+testtarget = 13195
+realtarget = 600851475143
 
-  maximum(primeset)
+## Attempt 1: a hard way
+function v1(target)
+    primeset = []
+    i = 2
+    while i < √target
+        if target % i == 0 && !any(i .% primeset .== 0)
+            append!(primeset, i)
+        end
+        i += 1
+    end
+
+    maximum(primeset)
 end
 
-v1(13195)
-v1(600851475143)
+v1(testtarget)
+v1(realtarget)
 
-## Attempt 2
-# using a package
-import Primes: factor
+## Attempt 2: using a package
+import Primes:factor
 
 function v2(target)
-  maximum(factor(Vector, target))
+    maximum(factor(Vector, target))
 end
 
-v2(13195)
-v2(600851475143)
+v2(testtarget)
+v2(realtarget)
+
+## Attempt 3: Something more elegant
+function v3(target)
+    i = 2
+    while i < target
+        if target  % i == 0
+            target /= i
+        else
+            i += 1
+        end
+    end
+
+    i
+end
+
+v3(testtarget)
+v3(realtarget)
 
 ##
-println("Attempt 1: ", v1(600851475143))
-println("Attempt 2: ", v2(600851475143))
+println("Attempt 1: ", v1(realtarget))
+println("Attempt 2: ", v2(realtarget))
+println("Attempt 3: ", v3(realtarget))
+
+## Another test
+v1(84)
+v2(84)
+v3(84)
+v1(84) == v2(84) == v3(84)
